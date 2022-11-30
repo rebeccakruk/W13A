fdba75fcd36afff3e5b25b64b6e08e49f40ad84e
rk <rebeccakruk@outlook.com>
2022-11-29 21:50:36 -0700

    added content to components

---
+++ src/components/body.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h3>Consistency is wonderful! Intro to why Frameworks are good.</h3>
+        <p>VueJS forces everyone to write in the same style of code.</p>
+        <p>The code is written in component based architecture.</p>
+        <h2>There are a couple of drawbacks, but not insurmountable challenges:</h2>
+        <p>Frameworks have a steeper learning curve and you have to know what you're doing to be proficient with them.</p>
+        <p>Frameworks have rigid structures, so you must follow the rules of Vue or it will not work!</p>
+        <img src="https: //assets.fireside.fm/file/fireside-images/podcasts/images/3/3911462c-bca2-48c2-9103-610ba304c673/guests/c/c3c2d98b-f149-4381-a0f6-6ab98d97474e/avatar_small.jpg" alt="Evan You">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "BodyComponent"    
+    }
+</script>
+
+<style scoped>
+h3 {
+    background-color: lightgray;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+img {
+    height: 20vh;
+}
+</style>
---
+++ src/components/footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h5>I hope this information was accurate. If not, I'll pay closer attention in class! haha</h5>
+        <p>Copyright 2022 - All Rights Reserved</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "FooterComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
