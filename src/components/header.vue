63d9a9773dc4c753d0c9427337b6f3e31e7832c5
rk <rebeccakruk@outlook.com>
2022-11-29 21:04:36 -0700

    created three component pages

---
+++ src/components/header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <header>
+            <h1>Welcome to my VueJS page!</h1>
+            <h2>Keep reading for some exciting things about VueJS</h2>
+        </header>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HeaderComponent"
+    }
+</script>
+
+<style scoped>
+h1 {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    text-shadow: 4pt;
+}
+</style>
